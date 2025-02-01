03d35c8d165480da82ebd7c8ae49c58d1942261b
codingdayo <145878788+codingdayo@users.noreply.github.com>
2025-02-02 00:52:22 +0100

    changes

--- src/main/java/codingdayo/hng_stage0/controller/UserController.java
+++ src/main/java/codingdayo/hng_stage0/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 
+
 @CrossOrigin(origins = "hngstage0-production-0acb.up.railway.app/", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1")
