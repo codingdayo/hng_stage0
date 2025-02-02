60b4c078d014003d0dbca06c3357dd5bc0d05879
codingdayo <145878788+codingdayo@users.noreply.github.com>
2025-02-02 19:23:45 +0100

    mapping

--- src/main/java/codingdayo/hng_stage0/controller/UserController.java
+++ src/main/java/codingdayo/hng_stage0/controller/UserController.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 
 @CrossOrigin(origins = "https://hngstage0-production-780b.up.railway.app/", maxAge = 3600)
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/v1/UserInfo")
 @RequiredArgsConstructor
 public class UserController {
 
